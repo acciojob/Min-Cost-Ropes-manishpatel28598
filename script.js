349229dbff8e7d639d78716bcfa22f3d0498c58b
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-02-29 10:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+let sum = 0; 
+let ans = []
+	while(arr.length>2){
+		arr.sort()
+		sum = arr[0]+arr[1]
+		arr.splice(0,2)
+		arr.push(sum)
+		ans.push(sum)
+	}
+  return ans.reduce((sum, elem)=>sum+elem,0)+arr[0]+arr[1]
 }
 
 module.exports=mincost;
