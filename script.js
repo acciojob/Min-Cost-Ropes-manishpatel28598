2521044f1363dd9e6ccd532a0d5a5bfc075a944a
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-02-29 10:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function mincost(arr)
 // return the min cost
 let sum = 0; 
 let ans = []
+	function compare(a,b){
+		return a-b
+	}
 	while(arr.length>2){
-		arr.sort()
+		arr.sort(compare)
 		sum = arr[0]+arr[1]
 		arr.splice(0,2)
 		arr.push(sum)
